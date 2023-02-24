bc74a52fc376a5fec21331fbd69077a8858edb99
dianexjeong <zuphyr14@gmail.com>
2023-02-24 11:33:31 +0900

    [GIF-383] feat: complete Section2

--- src/@components/organisms/Section2/index.tsx
+++ src/@components/organisms/Section2/index.tsx
@@ -12,21 +12,21 @@ export const Section2 = () => {
             <Text text="정답이 없는 직원 복지.." color={palette.Navy200} weight={600} size={36} height={160} />
             <Text text="이런 고민 해본 적 있으신가요?" color={palette.Navy200} weight={600} size={36} height={160} />
             <Space margin="29.13px 0px 0px 0px" />
-            <Flex direction="row" gap={189.13}>
+            <Flex direction="row" gap={185} height="108px">
               <Flex align="flex-start">
-                <Text text="직원들이" color={palette.Gray700} weight={400} size={22.5} height={160} />
-                <Text text="만족할 만한 복지가" color={palette.Gray700} weight={400} size={22.5} height={160} />
-                <Text text="무엇인지 모르겠어요" color={palette.Gray700} weight={400} size={22.5} height={160} />
+                <Text text="직원들이" color={palette.Gray700} weight={400} size={22} height={160} />
+                <Text text="만족할 만한 복지가" color={palette.Gray700} weight={400} size={22} height={160} />
+                <Text text="무엇인지 모르겠어요" color={palette.Gray700} weight={400} size={22} height={160} />
               </Flex>
               <Flex align="flex-start">
-                <Text text="직원들이" color={palette.Gray700} weight={400} size={22.5} height={160} />
-                <Text text="만족할 만한 복지가" color={palette.Gray700} weight={400} size={22.5} height={160} />
-                <Text text="무엇인지 모르겠어요" color={palette.Gray700} weight={400} size={22.5} height={160} />
+                <Text text="직원들이" color={palette.Gray700} weight={400} size={22} height={160} />
+                <Text text="만족할 만한 복지가" color={palette.Gray700} weight={400} size={22} height={160} />
+                <Text text="무엇인지 모르겠어요" color={palette.Gray700} weight={400} size={22} height={160} />
               </Flex>
               <Flex align="flex-start">
-                <Text text="직원들이" color={palette.Gray700} weight={400} size={22.5} height={160} />
-                <Text text="만족할 만한 복지가" color={palette.Gray700} weight={400} size={22.5} height={160} />
-                <Text text="무엇인지 모르겠어요" color={palette.Gray700} weight={400} size={22.5} height={160} />
+                <Text text="직원들이" color={palette.Gray700} weight={400} size={22} height={160} />
+                <Text text="만족할 만한 복지가" color={palette.Gray700} weight={400} size={22} height={160} />
+                <Text text="무엇인지 모르겠어요" color={palette.Gray700} weight={400} size={22} height={160} />
               </Flex>
             </Flex>
           </Flex>
