fe058e0e21f7e1e22108d030c223bb14a79c6fb8
dianexjeong <zuphyr14@gmail.com>
2023-02-24 17:47:38 +0900

    [GIF-383] feat: complete Section4

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { Navigation } from "~/@components/molecules";
-import { Section1, Section2, Section3 } from "~/@components/organisms";
+import { Section1, Section2, Section3, Section4 } from "~/@components/organisms";
 
 const Home = () => {
   return (
@@ -8,6 +8,7 @@ const Home = () => {
       <Section1 />
       <Section2 />
       <Section3 />
+      <Section4 />
     </>
   );
 };
---
+++ src/@components/organisms/Section4/index.tsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import { Text, Flex } from "~/@components/atoms";
+import palette from "~/@styles/colors";
+
+export const Section4 = () => {
+  return (
+    <>
+      <Bg>
+        <InnerContainer>
+          <Flex justify="center" align="center">
+            <span>
+              <Text
+                text="취향이 다양한 직원들"
+                gradient={true}
+                background="linear-gradient(100.98deg, #C579DB 0.89%, #7C83E9 38.04%)"
+                weight={700}
+                size={36}
+                height={160}
+              />
+              <Text text="을 위해," weight={700} size={36} height={160} color={palette.Navy200} />
+            </span>
+            <Text text="직접 선택하는 개인화 복지 제공" weight={700} size={36} height={160} color={palette.Navy200} />
+          </Flex>
+        </InnerContainer>
+      </Bg>
+    </>
+  );
+};
+
+const Bg = styled.div`
+  width: 100vw;
+  height: 53vh;
+  min-height: 334px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const InnerContainer = styled.div`
+  width: 62.5vw;
+  min-width: 800px;
+  height: 100%;
+`;
