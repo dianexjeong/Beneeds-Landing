d6b7a57183c1d0101e05949850f0d7be721fc4b9
dianexjeong <zuphyr14@gmail.com>
2023-02-27 13:30:04 +0900

    [GIF-383] chore: add document title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import GlobalStyle from "~/@styles/GlobalStyle";
 import { Weblayout } from "~/@components/template";
 import styled from "styled-components";
@@ -13,6 +14,9 @@ declare global {
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
+      <Head>
+        <title>Beneeds 베니즈</title>
+      </Head>
       <Weblayout>
         <GlobalStyle />
 
